9f195a45c48a6c25830ef65291145e0956c3cf00
Steve <53173048+JuniorSteve770@users.noreply.github.com>
2020-12-01 07:59:16 +0000

    Update Login.js

--- src/Components/00Landing/Login.js
+++ src/Components/00Landing/Login.js
@@ -21,7 +21,7 @@ function Login() {
 
     return (
         <div className="login">
-        <h2>Whatsapp clone 2020 </h2>
+        <h2>Whatsapp Build 2020 </h2>
             <div className ="login_container">
                 <img src="https://www.messengerpeople.com/wp-content/uploads/bfi_thumb/whatsapp-africa-32ilx9h511np7tuz88dwv959eto6oh605v6fxupc6hi35wei6.png" 
                         alt=""                    
